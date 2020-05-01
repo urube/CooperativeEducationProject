033572a1fcf0b3e7a1f0c5a23359916263330a77
urube <50559400+urube@users.noreply.github.com>
2020-05-01 21:54:41 +1200

    using @pytest.fixture (for mike)

--- pytest_best_practice.py
+++ pytest_best_practice.py
@@ -3,8 +3,6 @@ from box_toy import Box, Toy
 
 
 class TestEmptyBox:
-    # def setup(self):
-    #     self.box = Box()
 
     def test_a_empty_Box_toy_count(self):
         box = Box()
@@ -30,11 +28,25 @@ class TestEmptyBox:
 
 
 class TestToy:
-
+    # The purpose of test fixtures is to provide a fixed baseline upon
+    # which tests can reliably and repeatedly execute.
+    # Fixtures can be used to share test data between tests, execute setup
+    # and teardown methods before and after test executions respectively.
     @pytest.fixture(scope='session')
     def database(self):
-        data = [('Aardvark', 'Brown', 11.11)]
-        return data
+        param = [('Aardvark', 'Brown', 11.11)]
+        return param
+
+    # @pytest.fixture(scope='session', params=[('Aardvark', 'Brown', 11.11)])
+    # def database(self, request):  # Note: Using parameterized fixture
+    #     return request.param
+
+    # def setup(self, database):    # This line of code has error
+    #     for data in database:
+    #         var1 = data[0]
+    #         var2 = data[1]
+    #         var3 = data[2]
+    #         self.toy = Toy(var1, var2, var3)
 
     # @pytest.mark.parametrize('d0, d1, d2', data())
     def test_a_Toy_name(self, database):
@@ -80,10 +92,10 @@ class TestToyOfToys:
 
     @pytest.fixture(scope='session')
     def database(self):
-        data = [('Duck', 'Wooden', 33.33),
-                ('Aardvark', 'Brown', 11.11),
-                ('Doll', 'Pink', 22.22)]
-        return data
+        param = [('Duck', 'Wooden', 33.33) +
+                 ('Aardvark', 'Brown', 11.11) +
+                 ('Doll', 'Pink', 22.22)]
+        return param
 
     # @pytest.mark.parametrize('var1, var2, var3', database())
     # def setup_class(self, var1, var2, var3):
@@ -99,10 +111,16 @@ class TestToyOfToys:
             var1 = data[0]
             var2 = data[1]
             var3 = data[2]
+            var4 = data[3]
+            var5 = data[4]
+            var6 = data[5]
+            var7 = data[6]
+            var8 = data[7]
+            var9 = data[8]
             box = Box()
             box.add_toy(var1, var2, var3)
-            box.add_toy(var1, var2, var3)
-            box.add_toy(var1, var2, var3)
+            box.add_toy(var4, var5, var6)
+            box.add_toy(var7, var8, var9)
             assert box.toy_count == 3
 
     def test_b_full_Box_all_toys(self, database):
@@ -110,10 +128,17 @@ class TestToyOfToys:
             var1 = data[0]
             var2 = data[1]
             var3 = data[2]
+            var4 = data[3]
+            var5 = data[4]
+            var6 = data[5]
+            var7 = data[6]
+            var8 = data[7]
+            var9 = data[8]
             box = Box()
             box.add_toy(var1, var2, var3)
-            box.add_toy(var1, var2, var3)
-            box.add_toy(var1, var2, var3)
+            box.add_toy(var4, var5, var6)
+            box.add_toy(var7, var8, var9)
+            assert box.toy_count == 3
             assert box.toy_count == 3
             assert len(box.all_toys) == 3
 
@@ -122,8 +147,17 @@ class TestToyOfToys:
             var1 = data[0]
             var2 = data[1]
             var3 = data[2]
+            var4 = data[3]
+            var5 = data[4]
+            var6 = data[5]
+            var7 = data[6]
+            var8 = data[7]
+            var9 = data[8]
             box = Box()
             box.add_toy(var1, var2, var3)
+            box.add_toy(var4, var5, var6)
+            box.add_toy(var7, var8, var9)
+            assert box.toy_count == 3
             assert str(box) == 'This box has 3 toys\n' \
                    + '\tAardvark (Brown) @ $11.11\n' \
                    + '\tDoll (Pink) @ $22.22\n' \
@@ -132,3 +166,4 @@ class TestToyOfToys:
 
 if __name__ == '__main__':
     pytest.main(['-v', '-s', 'pytest_best_practice.py'])
+
