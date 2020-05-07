c6de694fe9d59dd7d71696c7e6d7d87ebbe15174
urube <50559400+urube@users.noreply.github.com>
2020-05-07 22:06:39 +1200

    Delete donation.py

--- blood_bank_PyTest/donation.py
+++
@@ -1,9 +0,0 @@
-class Donation:
-    def __init__(self, new_branch_name, new_town, new_when, new_blood_group):
-        self.branch_name = new_branch_name
-        self.town = new_town
-        self.when = new_when
-        self.blood_group = new_blood_group
-
-    def __str__(self):
-        return f'has blood group < {self.blood_group}> donated on {self.when} at {self.branch_name}'
